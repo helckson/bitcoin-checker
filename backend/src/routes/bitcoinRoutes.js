13bbac584f63b3e23e95a6aa80f09d8ce0cee86d
helckson <helckson.ishibashi@hotmail.com>
2024-08-24 18:35:15 -0300

    .

--- backend/src/routes/bitcoinRoutes.js
+++ backend/src/routes/bitcoinRoutes.js
@@ -3,7 +3,7 @@ const bitcoinController = require('../controllers/bitcoinController');
 
 const router = express.Router();
 
-router.get(itcoinController.getBitcoinPriceByDate);
+router.get(bitcoinController.getBitcoinPriceByDate);
 router.get(bitcoinController.getBitcoinPriceChange);
 
 module.exports = router;
